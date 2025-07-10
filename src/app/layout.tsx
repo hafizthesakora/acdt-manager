5a10c0415fb8921e57eb8ecce786d2471a9c3b1b
Hafiz Toyyib <hafizthesakora@gmail.com>
2025-07-10 12:33:26 +0000

    Implementing authenticating logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 
 import { cn } from '@/lib/utils';
 
+import { QueryProvider } from '@/components/query-provider';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -19,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, 'antialiased min-h-screen')}>
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
---
+++ src/features/auth/server/route.ts
@@ -0,0 +1,18 @@
+import { Hono } from 'hono';
+
+import { zValidator } from '@hono/zod-validator';
+import { loginSchema, registerSchema } from '../schemas';
+
+const app = new Hono()
+  .post('/login', zValidator('json', loginSchema), async (c) => {
+    const { email, password } = c.req.valid('json');
+    console.log('Login request received:', { email, password });
+    return c.json({ email, password });
+  })
+  .post('/register', zValidator('json', registerSchema), async (c) => {
+    const { name, email, password } = c.req.valid('json');
+    console.log('Register request received:', { name, email, password });
+    return c.json({ name, email, password });
+  });
+
+export default app;
