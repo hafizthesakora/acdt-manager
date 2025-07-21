0ff0bc5ae543fba99f0c87177b0f75e9660be59c
Hafiz Toyyib <hafizthesakora@gmail.com>
2025-07-21 11:35:37 +0000

    Setup for creating workspace

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 import { getCurrent } from '@/features/auth/actions';
+import { CreateWorkspaceForm } from '@/features/workspaces/components/create-workspace-form';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
@@ -6,5 +7,9 @@ export default async function Home() {
 
   if (!user) redirect('/sign-in');
 
-  return <div className="">This is a Homepage</div>;
+  return (
+    <div className="">
+      <CreateWorkspaceForm />
+    </div>
+  );
 }
