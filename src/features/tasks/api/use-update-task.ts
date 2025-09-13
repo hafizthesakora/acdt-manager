4a0ac8033c4449c4515d711f28a4bbfd89cd9515
Hafiz Toyyib <hafizthesakora@gmail.com>
2025-09-13 14:26:12 +0000

    Small fixes

--- src/features/tasks/api/use-update-task.ts
+++ src/features/tasks/api/use-update-task.ts
@@ -3,7 +3,6 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { InferRequestType, InferResponseType } from 'hono';
 
 import { client } from '@/lib/rpc';
-import { useRouter } from 'next/navigation';
 
 type ResponseType = InferResponseType<
   (typeof client.api.tasks)[':taskId']['$patch'],
@@ -14,7 +13,6 @@ type RequestType = InferRequestType<
 >;
 
 export const useUpdateTask = () => {
-  const router = useRouter();
   const queryClient = useQueryClient();
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json, param }) => {
@@ -31,10 +29,12 @@ export const useUpdateTask = () => {
     onSuccess: ({ data }) => {
       toast.success('Task updated');
 
-      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ['project-analytics'] });
+      queryClient.invalidateQueries({ queryKey: ['workspace-analytics'] });
       queryClient.invalidateQueries({ queryKey: ['tasks'] });
       queryClient.invalidateQueries({ queryKey: ['task', data.$id] });
     },
+
     onError: () => {
       toast.error('Failed to update tasks');
     },
